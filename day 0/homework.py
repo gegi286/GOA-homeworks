61ca474b98271a0391406e981f8b692cf46af86c
gegi286 <gegichkadua2@gmail.com>
2025-07-01 00:04:49 +0400

    first commit

---
+++ day 0/homework.py
@@ -0,0 +1,64 @@
+from turtle import *
+
+print("gegi chkadua")
+
+print(5)
+print("5")
+
+print(5+int("5"))
+
+#qweqweqweqwe
+
+print("gegi chkadua aris "+"5"+" wlis")
+#we want to paint a house 
+color("blue")
+width(7)
+speed(35)
+color("blue")
+begin_fill()
+
+
+
+
+
+
+forward(200)
+left(90)
+forward(200)
+left(45)
+forward(150)
+left(90)
+forward(150)
+left(45)
+forward (200)
+left(90)
+forward(20)
+forward(50)
+left(90)
+forward(150)
+right(90)
+forward(60)
+right(90)
+forward(150)
+
+penup()
+goto(201,201)
+right(90)
+pendown()
+forward(220)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exitonclick()
