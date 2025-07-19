7f9ff9cf6782273fb2d0cfa849ccd43a3059662c
gegi286 <gegichkadua2@gmail.com>
2025-07-19 20:04:35 +0400

    day 5 homework

---
+++ day 04/homework/task.py
@@ -0,0 +1,59 @@
+# task1
+# input - არის ფუნქცია, რომელიც საშუალებას გვაძლევს, მომხმარებელს კერძოდ ტერმინალში შევატანინოთ რაღაც მნიშვნელობა, მივიღოთ 
+# ამ მნიშვნელობაზე წვდომა და დავუმუშაოტ იგი.  მაგ: name = input ("enter favorite car: ")
+
+# output - არის პასუხი, რომელიც გამოგვაქ მომხმარებლისთვის რაღაც მნიშვნელობის სახით. მაგ: print(car)
+#task2
+var = input ("enter your age: ")
+print(var)
+ # task3
+var = input ("enter your age: ")
+print(type(var))
+# task4
+str1 = "nika" # string
+str2 = "fexburti" # string
+str3 = "cls63" # string
+str4 = "xe" # string
+str5 = "telefoni" # string
+
+int1 = 43 # integer
+int2 = 67 # integer
+int3 = 77 # integer
+int4 = 7 # integer
+int5 = 11 # integer
+
+float1 = 33.33 # float
+float2 = 44.44 # float
+float3 = 55.55 # float
+float4 = 66.66 # float
+float5 = 77.77 # float
+
+# task5
+
+var1 = "home"
+var2 = 88
+var3 = 11.11
+print(type(var1))
+print(type(var2))
+print(type(var3))
+
+# task 6
+name = input("enter your name: ")
+lastname = input("enter your lastname: ")
+print(name  +" " + lastname)
+
+# task7
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+num3 = int(input("enter third number: "))
+num4 = int(input("enter forth number: "))
+num5 = int(input("enter fifth number: "))
+print((num1 + num2 + num3 + num4 + num5)/5)
+
+# task8
+name = input("enter your name: ")
+lastname = input("enter your lastname: ")
+age = input("enter your age: ")
+wheight = input("enter your wheight: ")
+height = input("enter your height: ")
+print(name + " " +lastname + " " + age + " " + wheight + " " + height)
---
+++ day 05/homework/task.py
@@ -0,0 +1,57 @@
+# task1
+# chven viswavlet > < == მათი დანიშნულებაა რომ რიცხვები შეადარონ ერთმანეთს
+# task2
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == num2)
+
+# task3
+str1 = input()
+str2 = input()
+str3 = input()
+str4 = input()
+str5 = input()
+print(str1 + str2 + str3 + str4 + str5)
+
+# task4
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+num4 = int(input())
+print((num1 + num2 + num3 + num4)/4)
+# task5
+var1 = "mausi"
+var2 = 7
+var3 = 30.30
+var4 = False
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+# task6
+str1 = "ball"
+str2 = "pitch"
+print(str1 == str2)
+
+# task7
+str1 = "40"  
+str2 = "60"
+str3 = "50"
+str4 = "30"
+
+str1 = int(str1)
+str2 = int(str2)
+str3 = int(str3)
+str4 = int(str4)
+print(str1 + str2 + str3 + str4)
+# task8
+int1 = 44
+int2 = 55
+int3 = 66
+int1 = str(int1)
+int2 = str(int2)
+int3 = str(int3)
+print(int1 + int2 + int3)
+
