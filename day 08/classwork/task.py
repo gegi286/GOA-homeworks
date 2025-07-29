94c348a65a7b9d7f4dca8e1285907facd2f8e9c3
gegi286 <gegichkadua2@gmail.com>
2025-07-29 17:26:24 +0400

    day 08 homework

---
+++ day 08/classwork/task.py
@@ -0,0 +1,11 @@
+# task 1
+print(40 == 40 and 70 == 70)
+print(90 < 80 or 50 > 30)
+print(10 < 1000 and 50 == 50)
+print(10 < 10 or 90 > 100)
+print(60 == 80 and 50 ==40)
+print(20 < 10 and 40 == 30)
+# task2
+
+for i in range(20):
+    print("gegi chkadua")
---
+++ day 08/homework/task.py
@@ -0,0 +1,57 @@
+# task1
+print(90 == 90 or 70 > 70)
+print(20 > 10 and 30 == 30)
+print(40 < 50 or 100 > 90)
+print(11 > 12 or 60 < 90)
+print(44 > 33 and 20 == 20)
+print(33 > 44 and 33 < 44)
+print(90 == 99 or 66 >88)
+print(11 > 22 and 44 < 33)
+print(77 > 90 and 30 == 30)
+print(0 > 10 or 55 == 66)
+# task2
+# selection - არის არჩევა
+# sequencing - არის კოდის თანმიმდევრობით შესრულება
+#  iteration - ეს არის კოდის ბევრჯერ გამეორება
+# task3
+print("nika" * 10)
+print("goga" * 10)
+print("gegi" * 10)
+# ეს არის sequencing რადგან კოდი თანმიმდევრულად სრულდება
+# ჯერ 17 მერე 18 და მერე 19 ხაზი
+# task4
+# for loop  -არის ციკლი, გამეორება იგი გვეხმარება რომ კოდის რაღაცა ნაწილი ბევრჯერ გავიმეოროთ
+# task5
+# range ფუნქცია გვეხმარება რომ გავამეორებინოთ for loop ში მოცემული კოდი იმდენჯერ რამდენჯერაც ჩვენ გვინდა. მას ფრჩხილებში უნდა დავწეროთ გამეორების რაოდენობა
+# task6
+for i in range(7):
+    print("CLS 63")
+# task7
+for i in range(100):
+    print("CHKADUA")
+# task8
+for i in range(46):
+    print("black")
+# task9
+for i in range(32):
+    print("G")
+# task10
+str1 = input("enter your word: ")
+str2 = input("enter your word: ")
+str3 = input("enter your word: ")
+int1 = int(input("enter your number: "))
+print(str1 + str2 + str3 + str(int1))
+# task11
+var1 = "90"
+var2 = 33
+var3 = 44.44
+var4 = False
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+# task12
+int1 =int(input("enter your number: ")) 
+int2 =int(input("enter your number: "))
+int3 =int(input("enter your number: "))
+print(int1 + int2 + int3)
