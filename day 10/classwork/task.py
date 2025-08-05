fadc9fbf4b81cd8174a55035b9d443fc51b4299e
gegi286 <gegichkadua2@gmail.com>
2025-08-05 18:48:34 +0400

    day 10

---
+++ day 10/classwork/task.py
@@ -0,0 +1,7 @@
+# task1
+for i in range(50,100):
+    print(i)
+
+# task2
+for i in range(3,76):
+    print(i)
---
+++ day 10/homework/task.py
@@ -0,0 +1,41 @@
+
+# task1
+for i in range(0,39):   
+    print(i)
+# task2
+for i in range(30,40):
+    print(i)
+# task3
+for i in range(12,120):
+    print(i)
+# task4
+for i in range(18):
+    print("gegi")
+    print(14)
+# task5
+for i in range(10,100,3):
+    print(i)
+# task6
+for i in range(1,31):
+    print(str(i) + " ატამი")
+# task7
+age = input("enter your age: ")
+for i in range(14):
+    print(age)
+# task8
+i = 0
+while i < 10:
+    print("gegi")
+    i = i + 1
+# task9
+i = 0
+while i < 24:
+    print(i)
+    i = i + 1
+# task10
+for i in range(10,39,3):
+    print(i)
+i = 10
+while i < 39:
+    print(i)
+    i = i + 3
