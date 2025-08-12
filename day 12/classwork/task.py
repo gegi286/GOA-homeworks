a0d24b617784d4ccc3a19415564bb16b1245adb0
gegi286 <gegichkadua2@gmail.com>
2025-08-12 13:59:38 +0400

    day 12 homework

---
+++ day 12/classwork/task.py
@@ -0,0 +1,16 @@
+# task1
+for i in range(0,11):
+    print(i)
+
+# task2
+while i in range(5,20):
+    print(i)
+    
+
+# task3
+# Conditional statements --> pirobiti gancxadebebi
+
+# task4
+age = input("enter your age: ")
+if age > 18:
+    print("you are adult")
---
+++ day 12/homework/task.py
@@ -0,0 +1,25 @@
+ # task1
+ # Conditional statements --> pirobiti gancxadebebi aesebobs ori tipis esenia:if/else statementi 
+
+ # task2
+# for i in range(1,31):
+#      print(str(i) + " hello world")
+
+# task3
+i = 3,18
+while i in range(3,18):
+     print(i)
+
+
+
+
+
+    # task4
+var1 = input("enter your password: ")
+if ("password is 1234 Password is correct "):
+     print("password is incorrect")
+
+# task5
+dog = input("enter your favorite animal: ")
+print("woaf! woaf!")
+
