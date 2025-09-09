c629badeaad1afba141ea18a7dfba90e73535a88
gegi286 <gegichkadua2@gmail.com>
2025-09-09 18:23:16 +0400

    day 20 homework

---
+++ day 20/classwork/task.py
@@ -0,0 +1,12 @@
+# task1
+#  dges chven viswavlet: kvadratuli gamravleba anu ** , shemdegi nashtis gamotana anu %  da gayofa romelic gvichvenebs mocemul ricxvshi ramdenjer chaeteva hventvis sasurveli ricxvi
+# mag
+print(5 ** 3)
+print(15 //  4)
+print(12 %  9)
+# task2
+# siebshi cvladebisgan gansxvavebit gvexmaeba imashi rom siashi ra tipis monacemic gvinda ramdenjerme imas vinaxav da es cvladshi ar aris
+
+# task3
+your_list = ["gegi" , 7 , 1.1 , True]
+print(your_list)
---
+++ day 20/homework/task.py
@@ -0,0 +1,170 @@
+# task1
+# გამოკლება
+13 - 2 = 11
+44 - 22 = 22
+400 - 236 = 164
+# მიმატება
+77 + 44 = 121
+99 + 12 = 111
+7000 + 234 = 7234
+# გამრავლება
+3 * 4 = 12 
+7 * 10 = 70
+9 * 8 = 72
+# გაყოფა
+44 / 11 = 4
+9 / 3 = 3
+700 / 20 = 35
+# კვადრატული გამრავლება
+5 ** 3 = 125
+4 ** 4 = 256
+0 ** 333333 = 0
+# გაყოფა
+15 // 7 = 1
+55 // 44 = 1
+33 // 9 = 3
+# ნაშთიანი გაყოფა
+66 % 55 = 11
+30 % 15 = 0
+77 % 62 = 15
+
+# task2
+# chven viawavlet sam nairi matematikuri moqmedeba esenia:კვადრატული გამრავლება, გაყოფა da  ნაშთიანი გაყოფა
+# კვადრატული გამრავლება - am matematikur moqmedebas ayavs kvadratshi chventvis sssasurveli ricxvi
+# გაყოფა - es kvadratuli moqmedeba gvexmareba rom gavigo tu pirvel ricxvshi ramdenjer chaeteva meore ricxvi
+# ნაშთიანი გაყოფა - es kvadratuli moqmedeba gvexmareba rom gavigo roca pirrvli ricxvo gavyvet meoreze ramdeni darcheba nashti
+
+# task3
+5 ** 3 = 125
+4 ** 4 = 256
+0 ** 333333 = 0
+6 ** 3 = 216
+5 ** 2 = 25
+# გაყოფა
+15 // 7 = 1
+55 // 44 = 1
+33 // 9 = 3
+666 // 555 = 1
+398 // 378 = 1
+# ნაშთიანი გაყოფა
+66 % 55 = 11
+30 % 15 = 0
+77 % 62 = 15
+999 % 888 = 111
+489 % 459 = 30
+
+# task4
+15 / 3 = 5
+20 / 4 = 5
+100 / 20 = 5
+
+15 // 10 = 1
+10 // 7 = 1
+40 // 12 = 3
+
+4 * 3 = 12
+40 * 3 = 120
+120 * 3 = 360
+
+4 ** 3 = 64
+10 ** 3 = 1000
+2 ** 6 =64
+3 ** 4 = 81
+
+10 % 7 = 3
+3 % 2 = 1
+50 % 25 = 0
+14 % 11 = 3
+110 % 50 = 10
+
+# task5
+# siebs vuyenebt imistvis rom chven chveni sasurveli ram siashi chavwerot bevrjer es iqneba stringi,integeri,booleani da floati
+# siebi cvladebisgan gansxvavebit gvexmaeba imashi rom siashi ra tipis monacemic gvinda ramdenjerme imas vinaxav da es cvladshi ar aris
+
+#  task6
+my_list = ["gegi" , "7" , "araferi" ]
+# task7
+my_list = [7,10,11]
+# task8
+my_list = [11.11, 22.22, 33.33]
+# task9
+my_list = [True, False]
+# task10
+my_list = ["manqana" , 3 , 11.11 , True]
+
+# task11
+num1 = int(input("enter your first number: "))
+num2 = int(input("enter your second number: "))
+print(num1 ** num2)
+print(num1 // num2)
+print(num1 % num2)
+
+# task12
+num = int(input("enter your number: "))
+if 30 < num < 100:
+    print("between 30 and 100")
+elif 100 < num < 200:
+    print("between 100 and 200")
+else:
+    print("other number")
+
+# task13
+str = "cls"
+int = 7
+float = 11.11
+boolean = True
+print(type(str))
+print(type(int))
+print(type(float))
+print(type(boolean))
+
+# task14
+i = 50
+while i < 100:
+    i= i + 5
+    print(i)
+
+# task15
+for i in range(40,90,3):
+    print(i)
+
+# task16
+i = "Gegi Tchkadua"
+for i in range(15):
+    print(i)
+
+i = "Gegi Tchkadua"
+while i < 16:
+    i = i + 1
+    print(i)
+
+# task17
+name = "Gegi"
+surname = "Tchkadua"
+input("enter your name: ")
+if name == "Gegi":
+    print("enter your surname: ")
+elif surname == "Tchkadua":
+    print("same name and surname")
+elif surname != "Tchkadua":
+    print("same name but different surname")
+else:
+    print("aqedan moshordi")
+#  task18
+# შექმენი ცვლადი სადაც შეინახავ პაროლს
+# მომხმარებელს შემოატანინე პაროლი
+# სანამ პირობა არის ჭეშმარიტი(True)
+#       თუ მომხმარებლის პაროლი დაემთხვა შენს პაროლს:
+#             დაპრინტე --> გამოიცანი
+#              break
+#       სხვა შემთხვევაში:
+#           თავიდან გაუმეორე რომ შემოიყვანოს პაროლი
+password = "12345678"
+your_password = input("enter your password: ")
+while True:
+    if password == your_password:
+        print("გამოიცანი")
+        break
+    else:
+        your_password = input("enter your password: ")
+
