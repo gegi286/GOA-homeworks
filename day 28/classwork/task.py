5ae9b41afc677bdcdbb472168c0974ee4c67a655
gegi286 <gegichkadua2@gmail.com>
2025-10-05 15:38:51 +0400

    day 28 homework

---
+++ day 28/classwork/task.py
@@ -0,0 +1,14 @@
+# task1
+arr = [7, 8, 5, 3, 2, 6, 9, 1, 4, 7]
+print(arr[5:11])
+
+# task2
+arrr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+print(arrr[-20:-9])
+
+# task3
+my_list= [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+i = int(input("enter your number 0 - 11: "))
+
+num = my_list[2:i]
+print(num)
---
+++ day 28/homework/task.py
@@ -0,0 +1,54 @@
+# task1
+arr = ["gegi", "goga" , 6, "nika", 9, "GOA", "nika", "giorgi", 5, 0]
+print(arr[0:6])
+
+print(arr[7:10])
+
+print(arr[3:8])
+
+# task2
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+print(my_list[-15:-9])
+
+print(my_list[-9:])
+
+print(my_list[-9:-1])
+
+# task3 
+i = "თვითმფრინავი"
+print(i[8:])
+
+# task4
+p = "Programmer"
+sliced_p = p[0:3]
+sliced_p1 = p[-10:-7]
+print(sliced_p)
+print(sliced_p1)
+
+# task5
+week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+sliced_week = week[0:5]
+sliced_week1 = week[-7:-2]
+print(sliced_week)
+print(sliced_week1)
+
+sliced_week2 = week[5:]
+sliced_week3 = week[-3:-1]
+print(sliced_week2)
+print(sliced_week3)
+
+# task6
+py = "Python slicing is very powerful"
+sliced_py1 = py[0:6]
+sliced_py2 = py[7:14]
+sliced_py3 = py[23:]
+print(sliced_py1)
+print(sliced_py2)
+print(sliced_py3)
+
+sliced_py4 = py[-31:-25]
+sliced_py5 = py[-24:-17]
+sliced_py6 = py[-8:]
+print(sliced_py4)
+print(sliced_py5)
+print(sliced_py6)
