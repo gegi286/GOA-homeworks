38d270bf298a19ef8d07766f70d1b2f3f94bc8bf
gegi286 <gegichkadua2@gmail.com>
2025-10-14 14:10:27 +0400

    day

---
+++ day 29/homework/task.py
@@ -0,0 +1,70 @@
+# task1
+# მოცემულია სტრინგი "PythonProgramming".ამოიღე პირველი 6 სიმბოლო და დაბეჭდე გამოიყენეთ slicing
+
+py = "PythonProgramming"
+sliced_py = py[0:6]
+sliced_py1 = py[-17:-11]
+print(sliced_py)
+print(sliced_py1)
+
+# task2
+numbers = [10, 20, 30, 40, 50, 60, 70]
+sliced_numbers = numbers[2:5]
+sliced_numbers1 = numbers[-5:-2]
+print(sliced_numbers)
+print(sliced_numbers1)
+
+# task3
+# first index
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+index_letters = letters[1]
+index_letters1 = letters[-6]
+print(index_letters)
+print(index_letters1)
+
+# third index
+index_letters2 = letters[3]
+index_letters3 = letters[-4]
+print(index_letters2)
+print(index_letters3)
+
+# fifth index
+index_letters4 = letters[5]
+index_letters5 = letters[-2]
+print(index_letters4)
+print(index_letters5)
+
+
+# task4
+str = "HelloWorld"
+sliced_str = str[0:5]
+sliced_str1 = str[-10:-5]
+print(sliced_str)
+print(sliced_str1)
+
+# task5
+inf = "Information"
+sliced_inf = inf[2:7]
+sliced_inf1 = inf[-9:-4]
+print(sliced_inf)
+print(sliced_inf1)
+
+# task6
+# first
+a = "abcdefghijklmno"
+sliced_a = a[0:4]
+sliced_a1 = a[-15:-11]
+print(sliced_a)
+print(sliced_a1)
+
+# second
+sliced_a2 = a[9:]
+sliced_a3 = a[-6:]
+print(sliced_a2)
+print(sliced_a3)
+
+# third
+sliced_a4 = a[5:10]
+sliced_a5 = a[-10:-5]
+print(sliced_a4)
+print(sliced_a5)
