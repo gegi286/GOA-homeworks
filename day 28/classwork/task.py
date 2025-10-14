fab2e69690d40e974d6117b75dcec090613d03cb
gegi286 <gegichkadua2@gmail.com>
2025-10-14 14:01:58 +0400

    1

--- day 28/classwork/task.py
+++ day 28/classwork/task.py
@@ -7,8 +7,9 @@ arrr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
 print(arrr[-20:-9])
 
 # task3
-my_list= [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+my_list= [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 4]
 i = int(input("enter your number 0 - 11: "))
 
 num = my_list[2:i]
 print(num)
+
