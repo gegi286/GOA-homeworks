a01cb0b136d7d208513211d789aa75dbf9b38d08
gegi286 <gegichkadua2@gmail.com>
2025-11-09 16:27:37 +0400

    Update task.py

--- day 38/homework/task.py
+++ day 38/homework/task.py
@@ -1,46 +1,46 @@
-# # task1
-# name = input("enter your name: ")
+# task1
+name = input("enter your name: ")
 
-# uppered_name = name.upper()
+uppered_name = name.upper()
 
-# print(uppered_name)
+print(uppered_name)
 
-# # task2
-# name1 = input("enter your name: ")
+# task2
+name1 = input("enter your name: ")
 
-# lowwered_name1 = name1.lower()
+lowwered_name1 = name1.lower()
 
-# print(lowwered_name1)
+print(lowwered_name1)
 
-# # task3
-# name2 = input("enter your name: ")
+# task3
+name2 = input("enter your name: ")
 
-# capitalized_name2 = name2.capitalize()
+capitalized_name2 = name2.capitalize()
 
-# print(capitalized_name2)
+print(capitalized_name2)
 
-# # task4
-# names = ["nika", "gio", "goga", "saba", "gegi"]
-# for i in range(5):
-#     uppered_names = names[i].upper()
-#     print(uppered_names)
+# task4
+names = ["nika", "gio", "goga", "saba", "gegi"]
+for i in range(5):
+    uppered_names = names[i].upper()
+    print(uppered_names)
 
-# # task5
-# names1 = ["NIKA", "GIO", "GOGA", "SABA", "GEGI"]
-# for i in range(5):
-#     lowwered_names1 = names1[i].lower()
-#     print(lowwered_names1)
+# task5
+names1 = ["NIKA", "GIO", "GOGA", "SABA", "GEGI"]
+for i in range(5):
+    lowwered_names1 = names1[i].lower()
+    print(lowwered_names1)
 
-# # task6
-# my_list = ["nika", "gio", "goga", "saba", "gegi"]
-# for i in range(5):
-#     capitialzed_my_list = my_list[i].capitalize()
-#     print(capitialzed_my_list)
+# task6
+my_list = ["nika", "gio", "goga", "saba", "gegi"]
+for i in range(5):
+    capitialzed_my_list = my_list[i].capitalize()
+    print(capitialzed_my_list)
 
-# # task7
-# mine = ["7", "GOGA teacher", "car", "macivari", "nugzari"]
-# for i in range(5):
-#     print(len(mine[i]))
+# task7
+mine = ["7", "GOGA teacher", "car", "macivari", "nugzari"]
+for i in range(5):
+    print(len(mine[i]))
 
 # task8
 word = "ალექსანდრე"
