18a7028bf96f4b4232348864d82b8ff33761dc18
gegi286 <gegichkadua2@gmail.com>
2026-08-14 20:26:02 +0400

    homework

--- day 112/homework/task.py
+++ day 112/homework/task.py
@@ -2,10 +2,10 @@
 #     return int(bin(n)[2:])
 
 # task2
-binary_str = input("შეიყვანეთ ორობითი სტრინგი (მაგ. 101): ")
-decimal_number = 0
-for char in binary_str:
-    decimal_number = decimal_number * 2 + int(char)
-return decimal_number
+# binary_str = input("შეიყვანეთ ორობითი სტრინგი (მაგ. 101): ")
+# decimal_number = 0
+# for char in binary_str:
+#     decimal_number = decimal_number * 2 + int(char)
+# return decimal_number
 
 
