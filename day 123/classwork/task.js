09b8c912d286cbac177cbf3eaa28a91290116959
gegi286 <gegichkadua2@gmail.com>
2026-09-04 18:07:07 +0400

    homework

---
+++ day 123/classwork/task.js
@@ -0,0 +1,11 @@
+console.log(10)
+
+console.log(20)
+
+
+console.log('Gegi Tchkadua')
+console.log(17)
+console.log('Tbilisi-varketili III meore mikro raioni korpusi 4 bina 4 mestumret goga mas;)')
+console.log(1.80)
+console.log(10.88)
+
